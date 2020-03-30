dd8f360aaca071a168b0ab2f5af6cf189e0c60ab
Darrick Pang <dkypang@yahoo.com>
2020-03-30 19:49:02 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,10 +1,7 @@
 def reverse_each_word(str)
-  arr = str.split(' ')
-  newarr = []
-  x = 0 
-  while x < arr.length do 
-    newarr.push(arr[x].reverse)
-    x += 1
-  end 
-  newarr.join(' ')
+ arr = str.split(' ')
+
+ newarr = arr.collect{|x| x.reverse} 
+
+ newarr.join(' ')
 end 
